563b67c961ddc41a04574d9ad99688bcb68f3b89
dmiric <damir.miric@gmail.com>
2016-11-19 19:35:50 +0100

    dv_positions

---
+++ docroot/profiles/custom/dv/modules/custom/dv_positions/src/Form/PositionsDeleteForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\dv_positions\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+
+/**
+ * Provides a form for deleting Positions entities.
+ *
+ * @ingroup dv_positions
+ */
+class PositionsDeleteForm extends ContentEntityDeleteForm {
+
+
+}
