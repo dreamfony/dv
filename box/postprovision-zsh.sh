f77ec19f2c5a1eaf0d1af04395da5a06d052258f
dmiric <damir.miric@gmail.com>
2016-11-23 00:54:03 +0100

    add solr and zsh

---
+++ box/postprovision-zsh.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Install zsh
+#
+
+ZSH_SETUP_COMPLETE_FILE=/etc/zsh_install_complete
+
+# Check to see if we've already performed this setup.
+if [ ! -e "$ZSH_SETUP_COMPLETE_FILE" ]; then
+
+  sudo apt-get -y install zsh
+  sudo apt-get -y install zsh
+
+  sudo wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+
+  sudo chsh -s `which zsh` vagrant
+
+  # Create a file to indicate this script has already run.
+  sudo touch $ZSH_SETUP_COMPLETE_FILE
+else
+  exit 0
+fi
