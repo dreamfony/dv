563ad93eafd5914b1b6f5ef095a6008b722e001a
dmiric <damir.miric@gmail.com>
2016-12-01 00:45:06 +0100

    Turn off debugger for cli

---
+++ box/postprovision-tweaks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PHP_DIR="/etc/php/7.0"
+
+cd $PHP_DIR
+
+# remove xdebug form cli it can be enabled "sudo phpenmod -s cli xdebug"
+sudo mv $PHP_DIR/cli/conf.d/20-xdebug.ini $PHP_DIR/mods-available/xdebug.ini
+
+sudo rm $PHP_DIR/fpm/conf.d/20-xdebug.ini
+sudo phpenmod -s fpm xdebug
+
+
+
+
+
