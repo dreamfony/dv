19827ec959bf8dd22cfd73c80dbf07a00fab6c3c
dmiric <damir.miric@gmail.com>
2017-01-24 15:56:02 +0100

    dv_survey

---
+++ docroot/profiles/dv/modules/custom/dv_survey/src/SurveyGenerate.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\dv_survey;
+
+use Drupal\webform\Entity\Webform;
+
+/**
+ * Class SurveyGenerate
+ *
+ * @package Drupal\dv_survey
+ */
+class SurveyGenerate {
+
+  /**
+   * Creates a group. Adds entity to a group.
+   *
+   */
+  public function generate() {
+
+
+  }
+
+}
