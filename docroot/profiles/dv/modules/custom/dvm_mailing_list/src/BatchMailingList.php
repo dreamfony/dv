9b8c50d79983dc3a0485ada27e5ca523f620fb22
dmiric <damir.miric@gmail.com>
2017-02-06 22:17:06 +0100

    additional batch work

--- docroot/profiles/dv/modules/custom/dvm_mailing_list/src/BatchMailingList.php
+++ docroot/profiles/dv/modules/custom/dvm_mailing_list/src/BatchMailingList.php
@@ -6,11 +6,32 @@ use Drupal\group\Entity\GroupContent;
 use Drupal\group\Plugin\GroupContentEnabler\GroupMembership;
 use Drupal\group\Entity\Group;
 use Drupal\user\Entity\User;
+use Drupal\node\Entity\Node;
 
 
 class BatchMailingList {
 
+  /**
+   * Clean all content in the group.
+   *
+   * @param $group_id
+   * @param $context
+   */
+  public static function cleanGroup($group_id, &$context) {
+    $group = Group::load($group_id);
+    $group_content = $group->getContentEntities();
+    foreach ($group_content as $gc) {
+      /** @var GroupContent $gc */
+      $gc->delete();
+    }
 
+  }
+
+  /**
+   * @param $gids
+   * @param $group_id
+   * @param $context
+   */
   public static function addMembers($gids, $group_id, &$context) {
     if (!isset($context['sandbox']['progress'])) {
       $context['sandbox']['progress'] = 0;
@@ -18,56 +39,55 @@ class BatchMailingList {
 
       foreach ($gids as $gid) {
         $gid = $gid['target_id'];
-        /** @var Group $group */
-        $group = Group::load($gid);
-        $membership = $group->getMembers([$group->bundle() . '-organisation']);
 
-        foreach ($membership as $membershipgc) {
-          /** @var GroupMembership $membershipgc */
-          $org_uid[] = $membershipgc->getGroupContent()->getEntity()->id();
+        if ($gid !== NULL) {
+          /** @var Group $group */
+          $group = Group::load($gid);
+          $membership = $group->getMembers([$group->bundle() . '-organisation']);
+
+          foreach ($membership as $membershipgc) {
+            /** @var GroupMembership $membershipgc */
+            $org_uid[] = $membershipgc->getGroupContent()->getEntity()->id();
+          }
         }
       }
       $context['sandbox']['orgs'] = $org_uid;
       $context['sandbox']['max'] = count($org_uid);
     }
 //    process 5 items in one run
-    $context['sandbox']['process']+= 3;
+    $context['sandbox']['process'] += 3;
 
     $group = Group::load($group_id);
 
     do {
       $org_user_uid = array_pop($context['sandbox']['orgs']);
       $org_user = User::load($org_user_uid);
-      $group->addMember($org_user, ['group_roles' => [$group->bundle().'-organisation']]);
+      $group->addMember($org_user, ['group_roles' => [$group->bundle() . '-organisation']]);
 
       $context['sandbox']['progress']++;
-    } while (!empty($context['sandbox']['orgs']) AND ($context['sandbox']['progress'] != $context['sandbox']['process']));
+    } while (!empty($context['sandbox']['orgs']) && ($context['sandbox']['progress'] != $context['sandbox']['process']));
 
+  }
 
-// Check if we should finish
-    if (!empty($context['sandbox']['orgs'])) {
-      $context['finished'] = 0;
+  /**
+   * @param $nids
+   * @param $group_id
+   * @param $context
+   */
+  public static function addIssues($nids, $group_id, &$context) {
+
+    /// TODO make batch work
+    $group = Group::load($group_id);
+
+    foreach ($nids as $nid) {
+      $nid = $nid['target_id'];
+      /** @var Group $group */
+      $node = Node::load($nid);
+      $group->addContent($node, 'group_node:issue');
     }
-    else {
-      $context['finished'] = 1;
-    }
+
+    $context['finished'] = 1;
   }
-
-  public static function addIssues($nids, &$context) {
-
-
-  }
-
-  public static function cleanupIssues(&$context) {
-
-
-  }
-
-  public static function cleanupMembers(&$context) {
-
-
-  }
-
 
   public static function deleteNodeExampleFinishedCallback($success, $results, $operations) {
     // The 'success' parameter means no fatal PHP errors were detected. All
