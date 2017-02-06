d1716b3bc047a72a25c8d18eea3abfcbcbbd0591
tomislav <tomislav@dreamfony.com>
2017-02-06 21:19:20 +0100

    Batch add user to group

--- docroot/profiles/dv/modules/custom/dvm_mailing_list/src/BatchMailingList.php
+++ docroot/profiles/dv/modules/custom/dvm_mailing_list/src/BatchMailingList.php
@@ -3,9 +3,9 @@
 namespace Drupal\dvm_mailing_list;
 
 use Drupal\group\Entity\GroupContent;
+use Drupal\group\Plugin\GroupContentEnabler\GroupMembership;
 use Drupal\node\Entity\Node;
 use Drupal\group\Entity\Group;
-
 
 
 class BatchMailingList {
@@ -15,28 +15,40 @@ class BatchMailingList {
 
   }
 
-  public static function getMembership($gids, &$context) {
+  public static function getMembership($gids, $group_id, &$context) {
     if (!isset($context['sandbox']['progress'])) {
       $context['sandbox']['progress'] = 0;
-      $context['sandbox']['current_node'] = 0;
+      $context['sandbox']['process'] = 0;
 
       foreach ($gids as $gid) {
         $gid = $gid['target_id'];
         /** @var Group $group */
         $group = Group::load($gid);
-
-        $membership = $group->getMembers([$group->bundle().'-organisation']);
+        $membership = $group->getMembers([$group->bundle() . '-organisation']);
 
         foreach ($membership as $membershipgc) {
-          /** @var GroupContent $membershipgc */
-          $org_uid[] = $membershipgc->getEntity()->id();
-
+          /** @var GroupMembership $membershipgc */
+          $org_uid[] = $membershipgc->getGroupContent()->getEntity()->id();
         }
-
-
-
       }
-      $context['sandbox']['max'] = count($data['gid']);
+      $context['sandbox']['orgs'] = $org_uid;
+      $context['sandbox']['max'] = count($org_uid);
+    }
+//    process 5 items in one run
+    $context['sandbox']['process']+= 3;
+    do {
+      $org_user = array_pop($context['sandbox']['orgs']);
+//      TODO add $org_user to $group_id
+      $context['sandbox']['progress']++;
+    } while (!empty($context['sandbox']['orgs']) AND ($context['sandbox']['progress'] != $context['sandbox']['process']));
+
+
+// Check if we should finish
+    if (!empty($context['sandbox']['orgs'])) {
+      $context['finished'] = 0;
+    }
+    else {
+      $context['finished'] = 1;
     }
   }
 
