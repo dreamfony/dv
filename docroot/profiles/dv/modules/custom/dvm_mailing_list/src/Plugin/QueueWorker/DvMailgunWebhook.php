9ebf4c3d2ad928cdbb01307b47d70580417ddc80
tomislav <tomislav@dreamfony.com>
2017-02-14 22:24:31 +0100

    Update Mailgun Webhook events.

--- docroot/profiles/dv/modules/custom/dvm_mailing_list/src/Plugin/QueueWorker/DvMailgunWebhook.php
+++ docroot/profiles/dv/modules/custom/dvm_mailing_list/src/Plugin/QueueWorker/DvMailgunWebhook.php
@@ -28,26 +28,35 @@ class DvMailgunWebhook extends QueueWorkerBase {
   public function processItem($data) {
 
     /**
-     * bounce, deliver, drop, spam, unsubscribe, click, open
+     * accepted    Mailgun accepted the request to send/forward the email and the message has been placed in queue.
+     * rejected    Mailgun rejected the request to send/forward the email.
+     * delivered    Mailgun sent the email and it was accepted by the recipient email server.
+     * failed    Mailgun could not deliver the email to the recipient email server.
+     * opened    The email recipient opened the email and enabled image viewing. Open tracking must be enabled in the Mailgun control panel, and the CNAME record must be pointing to mailgun.org.
+     * clicked    The email recipient clicked on a link in the email. Click tracking must be enabled in the Mailgun control panel, and the CNAME record must be pointing to mailgun.org.
+     * unsubscribed    The email recipient clicked on the unsubscribe link. Unsubscribe tracking must be enabled in the Mailgun control panel.
+     * complained    The email recipient clicked on the spam complaint button within their email client. Feedback loops enable the notification to be received by Mailgun.
+     * stored    Mailgun has stored an incoming message
      */
 
     switch ($data['event']) {
-      case 'deliver':
+      case 'delivered':
         $status = ACTIVITY_STATUS_SENT;
         break;
-      case 'drop':
+      case 'failed':
+      case 'rejected':
         $status = ACTIVITY_STATUS_DELIVERY_ERROR;
         break;
 
-      case 'spam':
+      case 'complained':
         $status = ACTIVITY_STATUS_REJECTED;
         break;
 
-      case 'unsubscribe':
+      case 'unsubscribed':
         $status = ACTIVITY_STATUS_REJECTED;
         break;
 
-      case 'click':
+      case 'clicked':
         $status = ACTIVITY_STATUS_SEEN;
         break;
 
@@ -66,7 +75,8 @@ class DvMailgunWebhook extends QueueWorkerBase {
         ->getStorage('activity')
         ->load($data['entity_id']);
       $activity->set('field_activity_status', $status);
-      $activity->revision_log = $data;
+      $activity->revision_log = serialize($data);
+      $activity->setNewRevision();
       $activity->save();
 
     }
