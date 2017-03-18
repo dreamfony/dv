75a83907241c5c316d18c8d985e2235eaee01c24
dmiric <damir.miric@gmail.com>
2017-03-18 14:59:00 +0100

    Move batch to experimantal code

---
+++ docroot/profiles/dv/modules/experimental/batch/batch_submit.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Trigger batch on approved state.
+ *
+ * @param $form
+ * @param \Drupal\Core\Form\FormStateInterface $form_state
+ */
+function _dvm_mailing_list_group_form_submit($form, \Drupal\Core\Form\FormStateInterface $form_state) {
+
+  $group = $form_state->getFormObject()->getEntity();
+  $mod_state = $group->get('moderation_state')->getValue()[0]['value'];
+
+  if ($mod_state === 'published') {
+
+    /** @var \Drupal\Core\Url $redirect */
+    $redirect = $form_state->getRedirect();
+    $group_id = $redirect->getRouteParameters()['group'];
+
+// getMembership gids
+    $gids = $form_state->getValue('field_ml_to');
+
+//  getIssue nids
+    $group = Group::load($group_id);
+    $nids = $group->get('field_ml_questions')->getValue();
+
+    $batch = array(
+      'title' => t('Set up Survey...'),
+      'operations' => array(
+        array(
+          '\Drupal\dvm_mailing_list\BatchMailingList::cleanGroup',
+          array($group_id)
+        ),
+        array(
+          '\Drupal\dvm_mailing_list\BatchMailingList::addMembers',
+          array($gids, $group_id)
+        ),
+        array(
+          '\Drupal\dvm_mailing_list\BatchMailingList::addQuestions',
+          array($nids, $group_id)
+        ),
+      ),
+      'init_message' => t('Example Batch is starting.'),
+      'progress_message' => t('Processed @current out of @total.'),
+      'error_message' => t('Example Batch has encountered an error.'),
+      'finished' => '\Drupal\dvm_mailing_list\BatchMailingList::deleteNodeExampleFinishedCallback',
+    );
+    batch_set($batch);
+  }
+
+}
