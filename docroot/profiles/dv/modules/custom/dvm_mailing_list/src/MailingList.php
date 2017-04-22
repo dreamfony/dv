30247768c02584ab9201e35763488b83b21d95dc
dmiric <damir.miric@gmail.com>
2017-04-22 19:07:58 +0200

    Mailing List Set Display after all Activities have been created.

--- docroot/profiles/dv/modules/custom/dvm_mailing_list/src/MailingList.php
+++ docroot/profiles/dv/modules/custom/dvm_mailing_list/src/MailingList.php
@@ -10,9 +10,9 @@ use Drupal\group\Entity\Group;
 use Drupal\node\Entity\Node;
 use Drupal\group\GroupMembershipLoaderInterface;
 use Drupal\group\GroupMembership;
-use Drupal\panelizer\Panelizer;
 use Drupal\panelizer\PanelizerInterface;
 use Drupal\user\Entity\User;
+use Drupal\Core\Cache\CacheBackendInterface;
 
 
 /**
@@ -46,6 +46,17 @@ class MailingList {
    */
   protected $panelizer;
 
+  /**
+   * @var CacheBackendInterface
+   *   Cache backend.
+   */
+  protected $cacheBackend;
+
+  /**
+   * @var \Drupal\dvm_mailing_list\MailingListAnswer
+   */
+  protected $mailingListAnswer;
+
 
   /**
    * MailingList constructor.
@@ -53,11 +64,14 @@ class MailingList {
    * @param \Drupal\group\GroupMembershipLoaderInterface $group_membership_loader
    * @param \Drupal\activity_creator\Plugin\Type\ActivityActionManager $activity_action_manager
    * @param \Drupal\panelizer\PanelizerInterface $panelizer
+   * @param \Drupal\Core\Cache\CacheBackendInterface $cache_backend
    */
-  public function __construct(GroupMembershipLoaderInterface $group_membership_loader, ActivityActionManager $activity_action_manager, PanelizerInterface $panelizer) {
+  public function __construct(GroupMembershipLoaderInterface $group_membership_loader, ActivityActionManager $activity_action_manager, PanelizerInterface $panelizer, CacheBackendInterface $cache_backend, MailingListAnswer $mailing_list_answer) {
     $this->groupMembershipLoader = $group_membership_loader;
     $this->activityActionProcessor = $activity_action_manager;
     $this->panelizer = $panelizer;
+    $this->cacheBackend = $cache_backend;
+    $this->mailingListAnswer = $mailing_list_answer;
 
     $this->mailingListLabel = 'New Survey';
     $this->mailingListType = 'mailing_list';
@@ -201,31 +215,46 @@ class MailingList {
   }
 
   /**
-   * Check that all activites for mailing list have been created
-   * So that we can switch display mode that shows activites
-   *
-   * @param \Drupal\group\Entity\Group $group
-   * @return bool
+   * @param $group_id
+   * @return false|int|object
    */
+  public function allActivitiesCount($group_id) {
+    if($count = $this->cacheBackend->get('dvm_mailing_list:total_activity_count:'.$group_id)) {
+      return $count->data;
+    } else {
+      $group = Group::load($group_id);
+      $group_content_questions = count($group->getContent('group_node:question'));
+      $group_users = count($group->getMembers([$group->bundle() . '-organisation']));
 
-  public function checkActivitesCreated(Group $group) {
-    $group_content_questions = count($group->getContent('group_node:question'));
-    $group_users = count($group->getMembers([$group->bundle() . '-organisation']));
+      $count = (int) $group_content_questions * $group_users;
 
-    $all_activites_count = (int) $group_content_questions * $group_users;
-    $mailing_list_answer = \Drupal::service('dvm_mailing_list.mailing_list_answer');
-    $current_activites_count = (int) $mailing_list_answer->getAnswerCount($group->id());
+      $this->cacheBackend->set('dvm_mailing_list:total_activity_count:'.$group->id(), $count);
 
-    if ($current_activites_count < $all_activites_count) {
-      return FALSE;
+      return $count;
     }
-    return TRUE;
   }
 
-  public function switchDisplay(Group $group, $view_mode = 'full') {
-    // set view display mode
-    /// @todo: this is only an example we need to do this after last activity for this
-    /// @todo: mailing list was created
+  /**
+   * Check that all activities for mailing list have been created
+   * So that we can switch display mode that shows activities
+   *
+   * @param $group_id
+   * @return bool
+   */
+  public function checkActivitiesCreated($group_id) {
+    $all_activities_count = $this->allActivitiesCount($group_id);
+    $current_activities_count = (int) $this->mailingListAnswer->getAnswerCount($group_id);
+    return $current_activities_count < $all_activities_count ? FALSE : TRUE;
+  }
+
+  /**
+   * Switch Display Mode.
+   *
+   * @param $group_id
+   * @param string $view_mode
+   */
+  public function switchDisplay($group_id, $view_mode = 'full') {
+    $group = Group::load($group_id);
     $panels_displays = $this->panelizer->getDefaultPanelsDisplays('group', 'mailing_list', $view_mode);
     $this->panelizer->setPanelsDisplay($group, $view_mode, NULL, $panels_displays['default']);
   }
