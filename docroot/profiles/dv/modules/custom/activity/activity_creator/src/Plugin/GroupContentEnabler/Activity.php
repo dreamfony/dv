6025eb0e84763423e51ffcfd1b4954112a33f592
dmiric <damir.miric@gmail.com>
2017-04-26 12:21:22 +0200

    Group Content Enabler for Activity Entity with bundles

--- docroot/profiles/dv/modules/custom/activity/activity_creator/src/Plugin/GroupContentEnabler/Activity.php
+++ docroot/profiles/dv/modules/custom/activity/activity_creator/src/Plugin/GroupContentEnabler/Activity.php
@@ -4,6 +4,9 @@ namespace Drupal\activity_creator\Plugin\GroupContentEnabler;
 
 use Drupal\group\Plugin\GroupContentEnablerBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\group\Entity\GroupInterface;
+use Drupal\activity_creator\Entity\ActivityType;
+use Drupal\Core\Url;
 
 /**
  * Provides a content enabler for activity.
@@ -14,20 +17,12 @@ use Drupal\Core\Form\FormStateInterface;
  *   description = @Translation("Adds activity to groups."),
  *   entity_type_id = "activity",
  *   pretty_path_key = "activity",
+ *   reference_label = @Translation("Id"),
+ *   reference_description = @Translation("The id of the activity to add to the group"),
  *   deriver = "Drupal\activity_creator\Plugin\GroupContentEnabler\ActivityDeriver"
  * )
  */
 class Activity extends GroupContentEnablerBase {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function defaultConfiguration() {
-    $config = parent::defaultConfiguration();
-    $config['entity_cardinality'] = 1;
-
-    return $config;
-  }
 
   /**
    * {@inheritdoc}
@@ -43,6 +38,71 @@ class Activity extends GroupContentEnablerBase {
     $form['entity_cardinality']['#description'] .= '<br /><em>' . $info . '</em>';
 
     return $form;
+  }
+
+  /**
+   * Retrieves the node type this plugin supports.
+   *
+   * @return \Drupal\activity_creator\Entity\ActivityTypeInterface
+   *   The node type this plugin supports.
+   */
+  protected function getActivityType() {
+    return ActivityType::load($this->getEntityBundle());
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getGroupOperations(GroupInterface $group) {
+    $account = \Drupal::currentUser();
+    $plugin_id = $this->getPluginId();
+    $type = $this->getEntityBundle();
+    $operations = [];
+
+    if ($group->hasPermission("create $plugin_id entity", $account)) {
+      $route_params = ['group' => $group->id(), 'plugin_id' => $plugin_id];
+      $operations["activity-create-$type"] = [
+        'title' => $this->t('Create @type', ['@type' => $this->getActivityType()->label()]),
+        'url' => new Url('entity.activity.create_form', $route_params),
+        'weight' => 30,
+      ];
+    }
+
+    return $operations;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getTargetEntityPermissions() {
+    $permissions = parent::getTargetEntityPermissions();
+    $plugin_id = $this->getPluginId();
+
+    // Add a 'view unpublished' permission by re-using most of the 'view' one.
+    $original = $permissions["view $plugin_id entity"];
+    $permissions["view unpublished $plugin_id entity"] = [
+        'title' => str_replace('View ', 'View unpublished ', $original['title']),
+      ] + $original;
+
+    return $permissions;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    $config = parent::defaultConfiguration();
+    $config['entity_cardinality'] = 1;
+    return $config;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function calculateDependencies() {
+    $dependencies = parent::calculateDependencies();
+    $dependencies['config'][] = 'activity.type.' . $this->getEntityBundle();
+    return $dependencies;
   }
 
 }
--- docroot/profiles/dv/modules/custom/activity/activity_creator/src/Plugin/GroupContentEnabler/ActivityDeriver.php
+++ docroot/profiles/dv/modules/custom/activity/activity_creator/src/Plugin/GroupContentEnabler/ActivityDeriver.php
@@ -3,6 +3,7 @@
 namespace Drupal\activity_creator\Plugin\GroupContentEnabler;
 
 use Drupal\Component\Plugin\Derivative\DeriverBase;
+use Drupal\activity_creator\Entity\ActivityType;
 
 class ActivityDeriver extends DeriverBase {
 
@@ -10,11 +11,16 @@ class ActivityDeriver extends DeriverBase {
    * {@inheritdoc}.
    */
   public function getDerivativeDefinitions($base_plugin_definition) {
-      $this->derivatives['activity'] = [
-        'entity_bundle' => 'activity',
-        'label' => t('Activity'),
-        'description' => t('Adds activity to groups.'),
-      ] + $base_plugin_definition;
+    foreach (ActivityType::loadMultiple() as $name => $activity_type) {
+      /** @var ActivityType $activity_type */
+      $label = $activity_type->label();
+
+      $this->derivatives[$name] = [
+          'entity_bundle' => $name,
+          'label' => t('Group activity') . " ($label)",
+          'description' => t('Adds %type content to groups both publicly and privately.', ['%type' => $label]),
+        ] + $base_plugin_definition;
+    }
 
     return $this->derivatives;
   }
