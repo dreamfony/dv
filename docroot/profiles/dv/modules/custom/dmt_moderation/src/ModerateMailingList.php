81b5ab3c318638b73120991ea781f3a17361e8b6
dmiric <damir.miric@gmail.com>
2017-04-27 01:04:09 +0200

    ModerationMailingList Todos and comments

--- docroot/profiles/dv/modules/custom/dmt_moderation/src/ModerateMailingList.php
+++ docroot/profiles/dv/modules/custom/dmt_moderation/src/ModerateMailingList.php
@@ -8,6 +8,9 @@ use Drupal\activity_creator\Entity\Activity;
 use Drupal\activity_creator\Plugin\Type\ActivityActionManager;
 
 
+/// @todo: fix typos activities not activites
+/// @todo: rename class to Moderation
+
 class ModerateMailingList {
 
 
@@ -20,15 +23,19 @@ class ModerateMailingList {
     $this->activityActionProcessor = $activity_action_manager;
   }
 
+  /// @todo: rename createModerationActivity accepts EntityInterface $entity
   public function openModerationTicket(Group $group) {
 //    Create Activity
     $create_action = $this->activityActionProcessor->createInstance('moderation_action');
     $create_action->create($group);
   }
 
+  /// @todo: same as above
   public function closeModerationTicket(Group $group) {
     $activites = $this->GetRelatedActivites($group->id(), 'open');
     foreach ($activites as $activity) {
+      /** @var Activity $activity */
+      // $activity->setModerationState('closed');
       $this->changeActivityState($activity);
     }
   }
@@ -37,9 +44,12 @@ class ModerateMailingList {
   *  We actually never know which event exactly created which activity.
   *  Probably in more complex scenario we could create event entity and use UUID
   */
-
   private function getRelatedActivites($group_id, $status, $bundle = 'moderation_activity') {
     $query = \Drupal::entityQuery('activity')
+        /// $query->addTag($entity_type.'_'.$entity_bundle):
+        /// $query->addMetaData($entity_id);
+        /// and then implement dvm_mailing_list_query_group_mailing_list_alter
+        /// to add this first condition
       ->condition('field_activity_mailing_list.target_id', $group_id)
       ->condition('type', $bundle);
 
@@ -54,6 +64,7 @@ class ModerateMailingList {
     return $activites;
   }
 
+  /* @see: line 38 Activity Entitiy already has that method */
   private function changeActivityState(Activity $activity, $state = 'closed') {
 
   }
