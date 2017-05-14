c519804709aab465e6af9f9f5516452b26afa08c
dmiric <damir.miric@gmail.com>
2017-05-15 00:49:35 +0200

    Social Theme, Social User, Social Profile, Social Core

---
+++ docroot/profiles/dv/modules/social/social_user/src/Routing/RouteSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\social_user\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Class RouteSubscriber.
+ *
+ * @package Drupal\social_user\Routing
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    // Set route for the login to point to the SocialUserLoginForm.
+    if ($route = $collection->get('user.login')) {
+      $route->setDefaults(array(
+        '_form' => '\Drupal\social_user\Form\SocialUserLoginForm',
+        '_title' => t('Log in')->render(),
+      ));
+    }
+    if ($route = $collection->get('user.pass')) {
+      $route->setDefaults(array(
+        '_form' => '\Drupal\social_user\Form\SocialUserPasswordForm',
+        '_title' => t('Reset your password')->render(),
+      ));
+    }
+    if ($route = $collection->get('user.reset.form')) {
+      $route->setDefault('_title', t('Set your password')->render());
+    }
+  }
+
+}
