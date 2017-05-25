aa3367fa5df59aa25af7ac1c515c4d21945cbfe0
dmiric <damir.miric@gmail.com>
2017-05-25 20:06:25 +0200

    Semi working bottom right comments.

---
+++ docroot/profiles/dv/modules/custom/dmt_group_comments/js/comment_box.js
@@ -0,0 +1,18 @@
+(function ($) {
+
+  'use strict';
+
+  Drupal.behaviors.commentBox = {
+    attach: function (context, settings) {
+
+      var $comment_box_comments = $('.comment-box-comments');
+
+      $comment_box_comments.collapse().removeClass('in');
+
+      $('.comment-box', context).click(function () {
+        $comment_box_comments.toggle();
+      });
+    }
+  };
+
+})(jQuery);
