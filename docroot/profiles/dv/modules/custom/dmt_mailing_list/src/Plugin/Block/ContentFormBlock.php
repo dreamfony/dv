0a4017fbd27585276a1aa5882d79801788dcb0a0
dmiric <damir.miric@gmail.com>
2017-10-14 09:51:42 +0200

    Retrieve content form block

---
+++ docroot/profiles/dv/modules/custom/dmt_mailing_list/src/Plugin/Block/ContentFormBlock.php
@@ -0,0 +1,53 @@
+<?php
+namespace Drupal\dmt_mailing_list\Plugin\Block;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
+/**
+ * Provides a 'Content form block' block.
+ *
+ * @Block(
+ *   id = "content_form_block",
+ *   admin_label = @Translation("Content form block"),
+ * )
+ */
+class ContentFormBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $values = array('type' => 'content');
+
+    $node = \Drupal::entityTypeManager()
+      ->getStorage('node')
+      ->create($values);
+
+    $form = \Drupal::entityTypeManager()
+      ->getFormObject('node', 'default')
+      ->setEntity($node);
+
+    $form = \Drupal::formBuilder()->getForm($form);
+
+    return $form;
+  }
+
+  /**
+   * @param \Drupal\Core\Session\AccountInterface $account
+   * @return \Drupal\Core\Access\AccessResult|\Drupal\Core\Access\AccessResultForbidden
+   */
+  public function blockAccess(AccountInterface $account) {
+    if(\Drupal::routeMatch()->getRouteName() != 'entity.group.canonical') {
+      return AccessResult::forbidden();
+    }
+
+    $group = \Drupal::routeMatch()->getParameter('group');
+
+    if($group) {
+      return AccessResult::allowedIf($group->hasPermission('edit group', $account));
+    }
+
+    return AccessResult::forbidden();
+  }
+
+}
