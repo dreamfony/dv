d6876c6c7211296758903176950ad5c56fffb467
dmiric <damir.miric@gmail.com>
2017-11-14 17:40:28 +0100

    Fix xdebug mumbojumbo

--- box/postprovision-tweaks.sh
+++ box/postprovision-tweaks.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
+
 alias drush="/var/www/dv/vendor/drush/drush/drush"
 
 PHP_DIR="/etc/php/7.1"
 
 cd $PHP_DIR
 
-if [ ! -e "$PHP_DIR/mods-available/xdebug.ini" ]; then
+sudo rm $PHP_DIR/mods-available/xdebug.ini
 
- # remove xdebug form cli it can be enabled "sudo phpenmod -s cli xdebug"
- sudo mv $PHP_DIR/cli/conf.d/20-xdebug.ini $PHP_DIR/mods-available/xdebug.ini
+# remove xdebug form cli it can be enabled "sudo phpenmod -s cli xdebug"
+sudo mv $PHP_DIR/cli/conf.d/20-xdebug.ini $PHP_DIR/mods-available/xdebug.ini
 
- sudo rm $PHP_DIR/fpm/conf.d/20-xdebug.ini
- sudo phpenmod -s fpm xdebug
+sudo rm $PHP_DIR/fpm/conf.d/20-xdebug.ini
+sudo phpenmod -s fpm xdebug
+sudo service php7.1-fpm restart
 
-else
-  exit 0
-fi
 
 
 
