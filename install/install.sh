46319dbb633ce557e790816d136dc93c53077d77
dmiric <damir.miric@gmail.com>
2017-11-29 19:22:54 +0100

    Improve installation:   - set php fpm user and group to current user   - move some commands from install.sh to playbook   - fix oh my zsh user

--- install/install.sh
+++ install/install.sh
@@ -25,22 +25,3 @@ ansible-galaxy install -r requirements.yml
 
 title "Provision playbook for $(whoami)"
 ansible-playbook -i "localhost" -c local playbook.yml
-
-cd ..
-
-title "Run Composer Install"
-composer install --no-interaction
-
-title "Remove PHP used only for Composer setup"
-sudo apt-get remove -y --purge "php*"
-sudo rm /etc/php -R
-
-cd /var/www/dv/install/lamp
-
-title "Provision server playbook"
-ansible-playbook playbook.yml
-
-cd /var/www/dv/
-touch blt/project.local.yml
-echo "environment: local" >> blt/project.local.yml
-vendor/bin/blt custom:reinstall
