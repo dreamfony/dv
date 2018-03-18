1f31a08e3b907e562f32b43005fd77889c57e30a
dmiric <damir.miric@gmail.com>
2018-03-18 17:43:26 +0100

    Wireframe add opacity slider.

--- docroot/profiles/dv/modules/environment/dev/wireframe_overlay/js/wireframe_overlay.js
+++ docroot/profiles/dv/modules/environment/dev/wireframe_overlay/js/wireframe_overlay.js
@@ -7,8 +7,6 @@
 
             if(wireframe_overlay) {
 
-                var display;
-
                 var toolbar = $('#toolbar-bar');
                 var toggle_link = $('<div class="toolbar-tab"><a href="#" class="wf-toggle toolbar-item">Wireframe</a></div>');
                 toolbar.once('toggle-link').append(toggle_link);
@@ -29,9 +27,33 @@
                 var wf_description = $('<p>', {class: "wf-description"}).text(drupalSettings.wireframe_overlay.description);
                 overlay_wrapper.once('wf-description').append(wf_description);
 
-                toggle_link.click(function () {
-                    overlay_wrapper.toggle(display);
+                // slider stuff
+                var wf_slider = $('<div>', {id: 'wf-slider'});
+
+                toolbar.once('wf-slider').append(wf_slider);
+
+                wf_slider.slider({
+                    range: "min",
+                    value: 37,
+                    min: 1,
+                    max: 100,
+                    slide: function( event, ui ) {
+                        wf_image.css('opacity', ui.value/100);
+                    }
                 });
+
+                var toggle_link_position = toggle_link.offset();
+
+                wf_slider.css('top', toggle_link_position.top + 28);
+                wf_slider.css('left', toggle_link_position.left - 20);
+
+                toggle_link.click(function () {
+                    overlay_wrapper.toggle();
+                    wf_image.css('opacity', 37/100);
+                    wf_slider.toggle();
+                });
+
+
 
             }
         }
